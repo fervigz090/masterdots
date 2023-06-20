d31d2f32b6e249a5e42d244b2a0dc6465b6923c1
Ivan Fernandez <ivan@MacBook-Pro-de-Ivan-2.local>
2023-06-20 09:51:35 +0200

    first commit

--- js/app.js
+++ js/app.js
@@ -7,3 +7,10 @@
 // Capturar el valor del input nick
 const nickInput = document.getElementById('nick');
 console.log(nickInput.nodeType);
+nickInput.value = "Paco";
+console.log(nickInput.value);
+
+// Capturar el valor del select
+const tamanoInput = document.getElementById('tamano');
+console.log(tamanoInput.value);
+console.log(tamanoInput.options[tamanoInput.selectedIndex].text);
