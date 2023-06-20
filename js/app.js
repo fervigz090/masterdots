1a38ec3d938a7317361f2b196e4a0294e8f8673c
Ivan Fernandez <ivan@MacBook-Pro-de-Ivan-2.local>
2023-06-20 18:18:19 +0200

    first commit

--- js/app.js
+++ js/app.js
@@ -7,10 +7,14 @@
 // Capturar el valor del input nick
 const nickInput = document.getElementById('nick');
 console.log(nickInput.nodeType);
-nickInput.value = "Paco";
 console.log(nickInput.value);
 
 // Capturar el valor del select
 const tamanoInput = document.getElementById('tamano');
 console.log(tamanoInput.value);
 console.log(tamanoInput.options[tamanoInput.selectedIndex].text);
+
+// Ejemplo sobre eventos
+function test(){
+    console.log("Evento sobre raton");
+}
