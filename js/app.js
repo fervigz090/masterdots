65b9fcc7a6dabd119ba460c12feb8e48bdce5afe
Ivan Fernandez <ivan@MacBook-Pro-de-Ivan-2.local>
2023-06-20 09:40:39 +0200

    first commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 //alert("Ventana horripilante");
+
+/**
+ * JS para la comprobación de datos del formulario de entrada
+ */
+
+// Capturar el valor del input nick
+const nickInput = document.getElementById('nick');
+console.log(nickInput.nodeType);
