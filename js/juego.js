f8c52afe20a1c1bed466954ba7ff14a46c0ab0bd
Ivan Fernandez <ivan@MacBook-Pro-de-Ivan-2.local>
2023-07-07 16:01:37 +0200

    efectos click items

--- js/juego.js
+++ js/juego.js
@@ -6,6 +6,15 @@
  * @date 2023-06-22
  */
 
+/**
+ * Devuelve un numero aleatorio entre 0 y max
+ * @date 2023-07-07
+ * @param { * } max limite maximo
+ */
+function getRamdomInt(max){
+    return Math.floor(Math.random() * max);
+}
+
 function rellenarFormularioUsuario(){
     document.getElementById("nick").value = nick;
     document.getElementById("avatarImg").src = avatarImg;
@@ -14,6 +23,45 @@ function rellenarFormularioUsuario(){
 function pintarPanelJuego(){
     document.getElementById("juego").style.gridTemplateColumns = "repeat("+tamano+", 1fr)";
     document.getElementById("juego").style.gridTemplateRows = "repeat("+tamano+", 1fr)";
+
+    //Elementos de forma automatica
+    let items = "";
+    let color = ["rojo", "verde"];
+    let colorRnd = 0;
+    for (let index = 0; index < (parseInt(tamano) * parseInt(tamano)); index++) {
+        if(index%2 > 0){
+            colorRnd = getRamdomInt(2);
+        }
+        items += `<div class="containerItem"><div class="item ${color[colorRnd]}"></div></div>`;
+    }
+    document.getElementById("juego").innerHTML = items;
+}
+
+/**
+ * Añadir los eventos al juego
+ * @date 2023-07-07
+ */
+function programarEventosJuego() {
+    const items = document.getElementsByClassName('item');
+    for (let item of items){
+        item.addEventListener('mousedown', comenzarMarcar);
+    }
+}
+
+/* Funciones del juego*/
+
+/**
+ * Iniciar el marcado de los dots
+ * @date 2023-07-07
+ * @param { * } event mousedown
+ */
+function comenzarMarcar(event) {
+    let item = event.target;
+    let containerItem = event.target.parentElement;
+    if(item.classList.contains('rojo')) containerItem.classList.add('rojo');
+    else containerItem.classList.add('verde');
+    
+    console.log("Pinchado sobre un circulo");
 }
 
 //Capturamos los datos del usuario
@@ -24,3 +72,5 @@ if(!comprobacionDatosUsuario()) location = "index.html";
 rellenarFormularioUsuario();
 
 pintarPanelJuego();
+
+programarEventosJuego();
